3cb84fa4f1ffa569a89661038772a460bf3e76d9
赵炳森 <44658494+474073456@users.noreply.github.com>
2020-10-18 20:50:23 +0800

    test2

---
+++ build/precache-manifest.f65f697fe3cce10c91653634dad43834.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9cb06fc1a85ac6de9938d48ad48d7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2664c07e66d24af228c0",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "a8ba94a6806c4f7ed65e",
+    "url": "/static/css/main.ad01974d.chunk.css"
+  },
+  {
+    "revision": "2664c07e66d24af228c0",
+    "url": "/static/js/2.98698563.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.98698563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ba94a6806c4f7ed65e",
+    "url": "/static/js/main.8d0b9228.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
